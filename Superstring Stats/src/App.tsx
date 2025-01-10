ca846488e4a6e30dc5780df89d0a33b42a3bad0d
sushiNathan <nathanmangaoil49@gmail.com>
2025-01-10 02:45:17 -0500

    Cleaning up unused files and references

--- Superstring Stats/src/App.tsx
+++ Superstring Stats/src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import HomePage from './pages/home'
 import FlaviaPage from './pages/flavia'
